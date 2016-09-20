16bc517b2e72ba46d658629ee75455ab34013e66
Max <xwason@gmail.com>
2016-09-20 10:32:38 -0700

    Update BlockPos import

--- src/main/java/gr8pefish/ironbackpacks/items/upgrades/UpgradeMethods.java
+++ src/main/java/gr8pefish/ironbackpacks/items/upgrades/UpgradeMethods.java
@@ -14,8 +14,8 @@ import net.minecraft.nbt.NBTTagByteArray;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 
