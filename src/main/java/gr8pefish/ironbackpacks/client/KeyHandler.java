ea79d7f93999af6392451052bca304c7c8662122
Max <xwason@gmail.com>
2016-09-23 09:33:12 -0700

    Fix localization for keybindings

--- src/main/java/gr8pefish/ironbackpacks/client/KeyHandler.java
+++ src/main/java/gr8pefish/ironbackpacks/client/KeyHandler.java
@@ -1,13 +1,14 @@
 package gr8pefish.ironbackpacks.client;
 
 import gr8pefish.ironbackpacks.api.Constants;
+import gr8pefish.ironbackpacks.util.TextUtils;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import org.lwjgl.input.Keyboard;
 
 public class KeyHandler {
-    public static final KeyBinding OPEN_PACK = new KeyBinding("key.ironbackpacks.open", Keyboard.KEY_C, Constants.MOD_NAME);
-    public static final KeyBinding EQUIP_PACK = new KeyBinding("key.ironbackpacks.equip", Keyboard.KEY_V, Constants.MOD_NAME); //TODO: localize
+    public static final KeyBinding OPEN_PACK = new KeyBinding(TextUtils.localize("key.ironbackpacks.open"), Keyboard.KEY_C, Constants.MOD_NAME);
+    public static final KeyBinding EQUIP_PACK = new KeyBinding(TextUtils.localize("key.ironbackpacks.equip"), Keyboard.KEY_V, Constants.MOD_NAME); //TODO: localize
 
     public static void init() {
         ClientRegistry.registerKeyBinding(OPEN_PACK);
