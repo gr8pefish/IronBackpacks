0641f11322589de2fc36262aa4e3b5904fb39677
gr8pefish <gr8pefish@gmail.com>
2016-11-21 20:52:24 -0700

    Fix dupe bug when double clicking an item stack with the configurable inventory open.
    Closes #104

--- src/main/java/gr8pefish/ironbackpacks/container/slot/GhostSlot.java
+++ src/main/java/gr8pefish/ironbackpacks/container/slot/GhostSlot.java
@@ -2,12 +2,13 @@ package gr8pefish.ironbackpacks.container.slot;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 
 /**
  * Used in the alternate gui to create a 'ghost items' that disappears when it is picked up and doesn't decrement the items stack when it is placed in.
  */
-public class GhostSlot extends BackpackSlot{
+public class GhostSlot extends BackpackSlot {
 
     public int index; //the slot index in the inventory
 
@@ -18,7 +19,7 @@ public class GhostSlot extends BackpackSlot{
 
     @Override
     public boolean canTakeStack(EntityPlayer player){
-        return true;
+        return false; //disallow things to take the slot (such as double click causing item duping)
     }
 
     @Override
