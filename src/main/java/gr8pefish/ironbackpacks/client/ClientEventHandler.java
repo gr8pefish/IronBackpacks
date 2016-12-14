504c59a2c12637a844d68c0d94d274c283a42b86
Max Wason <xwason@gmail.com>
2016-12-14 13:10:12 -0700

    Fix item loss from InvSorter with ghost slots
    Disabled scroll wheel entirely in that gui, unless it is used to move through advFilter slots
    Closes #119

--- src/main/java/gr8pefish/ironbackpacks/client/ClientEventHandler.java
+++ src/main/java/gr8pefish/ironbackpacks/client/ClientEventHandler.java
@@ -1,6 +1,8 @@
 package gr8pefish.ironbackpacks.client;
 
 import gr8pefish.ironbackpacks.client.gui.inventory.GUIBackpack;
+import gr8pefish.ironbackpacks.client.gui.inventory.GUIBackpackAlternate;
+import gr8pefish.ironbackpacks.container.slot.GhostSlot;
 import gr8pefish.ironbackpacks.integration.InterModSupport;
 import gr8pefish.ironbackpacks.items.backpacks.ItemBackpack;
 import gr8pefish.ironbackpacks.network.NetworkingHandler;
@@ -66,6 +68,46 @@ public class ClientEventHandler {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * Cancel scroll wheel when ghost slot inventory open to deal with InventorySorter.
+     *
+     * @param event - the mouse input event
+     */
+    @SubscribeEvent
+    public void onMouse(GuiScreenEvent.MouseInputEvent.Pre event) {
+        if (event.getGui() instanceof GUIBackpackAlternate) { //only alternate backpack gui
+
+            int wheelState = Mouse.getEventDWheel();
+            boolean canceled = (wheelState != 0); //if mouse wheel value here isn't 0, then the mouse wheel was scrolled (-120 for down and 120 for up for me)
+
+            //ToDo: occasionally dupes ghost items
+            //The following allows you to rotate the advanced filter with the scroll wheel
+            GUIBackpackAlternate gui = (GUIBackpackAlternate) event.getGui();
+            if (gui.hasFilterAdvancedUpgrade && wheelState != 0){
+                int x = Mouse.getEventX() * gui.width / gui.mc.displayWidth;
+                int y = gui.height - Mouse.getEventY() * gui.height / gui.mc.displayHeight - 1;
+
+                for (int i = 0; i < 9; i++) {
+                    GhostSlot slot = (GhostSlot) gui.container.getSlot(gui.container.getFilterAdvSlotIdStart() + i);
+                    if (gui.isMouseOverSlot(slot, x, y)) {
+                        canceled = false; //could just cancel always, but seems to mess up filter items more often
+                        if ((wheelState / 120) == 1) { //scroll up
+                            gui.container.changeAdvFilterSlots(IronBackpacksConstants.Miscellaneous.MOVE_RIGHT);
+                            NetworkingHandler.network.sendToServer(new SingleByteMessage(IronBackpacksConstants.Messages.SingleByte.MOVE_RIGHT));
+                            gui.drawButtons();
+                        } else { //scroll down
+                            if (gui.container.getSlot(gui.container.getFilterAdvSlotIdStart() + i - 1).getHasStack()) canceled = true; //slot to the left has stack, so it will rotate then pull out with InvSorter
+                            gui.container.changeAdvFilterSlots(IronBackpacksConstants.Miscellaneous.MOVE_LEFT);
+                            NetworkingHandler.network.sendToServer(new SingleByteMessage(IronBackpacksConstants.Messages.SingleByte.MOVE_LEFT));
+                            gui.drawButtons();
+                        }
+                    }
+                }
+            }
+            event.setCanceled(canceled);
         }
     }
 
