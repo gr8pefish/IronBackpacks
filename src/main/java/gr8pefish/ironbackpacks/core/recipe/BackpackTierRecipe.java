6b1826398dc5dd5db6ffd7591ba65ba7ceb39110
Nicholas Ignoffo <nick@tehnut.info>
2017-11-15 17:39:39 -0800

    Implement data carry over for backpack upgrades (#200)
    (cherry picked from commit 636acd6)
    Signed-off-by: Nicholas Ignoffo <nick@tehnut.info>

--- src/main/java/gr8pefish/ironbackpacks/core/recipe/BackpackTierRecipe.java
+++ src/main/java/gr8pefish/ironbackpacks/core/recipe/BackpackTierRecipe.java
@@ -1,8 +1,12 @@
 package gr8pefish.ironbackpacks.core.recipe;
 
+import gr8pefish.ironbackpacks.api.backpack.BackpackInfo;
+import gr8pefish.ironbackpacks.api.backpack.IBackpack;
 import gr8pefish.ironbackpacks.api.backpack.variant.BackpackSpecialty;
 import gr8pefish.ironbackpacks.api.backpack.variant.BackpackType;
 import gr8pefish.ironbackpacks.api.IronBackpacksAPI;
+import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
 import javax.annotation.Nonnull;
@@ -17,6 +21,18 @@ public class BackpackTierRecipe extends ShapedOreRecipe {
 
         this.resultType = resultType;
         this.resultSpecialty = resultSpecialty;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack getCraftingResult(@Nonnull InventoryCrafting matrix) {
+        ItemStack backpack = matrix.getStackInRowAndColumn(1, 1);
+        if (!(backpack.getItem() instanceof IBackpack))
+            return super.getCraftingResult(matrix);
+
+        ItemStack upgraded = getRecipeOutput().copy();
+        BackpackInfo upgradedInfo = BackpackInfo.upgradeTo(((IBackpack) backpack.getItem()).getBackpackInfo(backpack), getResultType(), getResultSpecialty());
+        return IronBackpacksAPI.applyPackInfo(upgraded, upgradedInfo);
     }
 
     public BackpackType getResultType() {
