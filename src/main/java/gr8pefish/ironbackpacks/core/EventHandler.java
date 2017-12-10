b2ac613894da65accfee81e3274338a52f750904
Nicholas Ignoffo <nick@tehnut.info>
2017-12-10 14:11:03 -0800

    Allow upgrades to be removed
    Temporary holdover until we implement a better system.
    
    Backpack + any shears in an anvil will remove the latest applied upgrade.
    No damage will be done to the anvil.
    
    Signed-off-by: Nicholas Ignoffo <nick@tehnut.info>

--- src/main/java/gr8pefish/ironbackpacks/core/EventHandler.java
+++ src/main/java/gr8pefish/ironbackpacks/core/EventHandler.java
@@ -1,6 +1,7 @@
 package gr8pefish.ironbackpacks.core;
 
 import gr8pefish.ironbackpacks.IronBackpacks;
+import gr8pefish.ironbackpacks.api.IronBackpacksAPI;
 import gr8pefish.ironbackpacks.api.backpack.BackpackInfo;
 import gr8pefish.ironbackpacks.api.backpack.IBackpack;
 import gr8pefish.ironbackpacks.api.upgrade.BackpackUpgrade;
@@ -8,14 +9,18 @@ import gr8pefish.ironbackpacks.api.upgrade.IUpgrade;
 import gr8pefish.ironbackpacks.capabilities.PlayerBackpackHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AnvilUpdateEvent;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.AnvilRepairEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.items.ItemHandlerHelper;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
@@ -35,10 +40,38 @@ public class EventHandler {
             event.setOutput(upgraded);
             event.setCost(1);
             event.setMaterialCost(1);
-            return;
+        } else if (event.getRight().getItem() instanceof ItemShears){
+            ItemStack removed = tryRemoveUpgrade(event.getLeft());
+            if (!removed.isEmpty()) {
+                event.setOutput(removed);
+                event.setCost(1);
+                event.setMaterialCost(0);
+            }
         }
+    }
 
-        // TODO - removing upgrade
+    @SubscribeEvent
+    public static void onAnvilPost(AnvilRepairEvent event) {
+        if (event.getItemResult().isEmpty() || !(event.getItemResult().getItem() instanceof IUpgrade))
+            return;
+
+        event.setBreakChance(0.0F); // If we're working with backpacks, let's not damage the anvil
+
+        if (event.getItemInput().isEmpty() || !(event.getItemInput().getItem() instanceof IBackpack))
+            return;
+
+        if (event.getIngredientInput().isEmpty() || !(event.getIngredientInput().getItem() instanceof ItemShears))
+            return;
+
+        BackpackUpgrade upgrade = ((IUpgrade) event.getItemResult().getItem()).getUpgrade(event.getItemResult());
+        IBackpack backpack = ((IBackpack) event.getItemInput().getItem());
+        BackpackInfo info = backpack.getBackpackInfo(event.getItemInput());
+
+        info.removeUpgrade(upgrade);
+        backpack.updateBackpack(event.getItemInput(), info);
+
+        ItemHandlerHelper.giveItemToPlayer(event.getEntityPlayer(), event.getItemInput());
+        ItemHandlerHelper.giveItemToPlayer(event.getEntityPlayer(), event.getIngredientInput());
     }
 
     @Nonnull
@@ -52,7 +85,7 @@ public class EventHandler {
         ItemStack output = left.copy();
 
         IBackpack backpack = (IBackpack) output.getItem();
-        BackpackInfo packInfo = ((IBackpack) output.getItem()).getBackpackInfo(output);
+        BackpackInfo packInfo = backpack.getBackpackInfo(output);
         BackpackUpgrade upgrade = ((IUpgrade) right.getItem()).getUpgrade(right);
 
         if (upgrade.isNull() || packInfo.getVariant().getBackpackType().isNull())
@@ -74,5 +107,19 @@ public class EventHandler {
         backpack.updateBackpack(output, packInfo);
 
         return output;
+    }
+
+    @Nonnull
+    private static ItemStack tryRemoveUpgrade(ItemStack left) {
+        if (left.isEmpty() || !(left.getItem() instanceof IBackpack))
+            return ItemStack.EMPTY;
+
+        BackpackInfo packInfo = ((IBackpack) left.getItem()).getBackpackInfo(left);
+        List<BackpackUpgrade> upgrades = packInfo.getUpgrades();
+        if (upgrades.isEmpty())
+            return ItemStack.EMPTY;
+
+        BackpackUpgrade upgrade = upgrades.get(upgrades.size() - 1);
+        return IronBackpacksAPI.getStack(upgrade);
     }
 }
