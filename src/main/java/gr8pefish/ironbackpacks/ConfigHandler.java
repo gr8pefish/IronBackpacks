b83167db5a20f903726c26f67aaf5a82926de542
Max Wason <xwason@gmail.com>
2018-05-22 15:24:11 -0700

    Fix config not updating
    Update changelog
    References #235

--- src/main/java/gr8pefish/ironbackpacks/ConfigHandler.java
+++ src/main/java/gr8pefish/ironbackpacks/ConfigHandler.java
@@ -1,13 +1,26 @@
 package gr8pefish.ironbackpacks;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = IronBackpacks.MODID, name = IronBackpacks.MODID + "/" + IronBackpacks.MODID)
+@Mod.EventBusSubscriber(modid = IronBackpacks.MODID)
 public class ConfigHandler {
 
     public static Upgrades upgrades = new Upgrades();
     public static Handholding handholding = new Handholding();
     public static BackpackSizes sizes = new BackpackSizes();
+
+    //Add config reloading
+    @SubscribeEvent
+    public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+        if (event.getModID().equals(IronBackpacks.MODID)) {
+            ConfigManager.sync(event.getModID(), Config.Type.INSTANCE); // Sync config values
+        }
+    }
 
     public static class Handholding {
         @Config.RangeInt(min = 1)
